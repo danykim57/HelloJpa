a4ccc63751da27cbe515b050264d9dbceee4d7de
YeonginKim <fhtmah12@gmail.com>
2023-01-02 22:32:37 +0900

    persistent, non-persistent example

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -14,12 +14,22 @@ public class JpaMain {
     tx.begin();
 
     try {
+      // 비영속
+      Member member = new Member();
+      member.setId(100L);
+      member.setName("HelloJPA");
+
+      // 영속
+      System.out.println("=== Before ");
+      em.persist(member);
+      em.detach(member);
+      System.out.println("After");
       List<Member> result = em.createQuery("select m from Member as m", Member.class)
           .setFirstResult(5)
           .setMaxResults(8)
           .getResultList();
-      for (Member member : result) {
-        System.out.println("member.name = " + member.getName());
+      for (Member memb : result) {
+        System.out.println("member.name = " + memb.getName());
       }
       tx.commit();
     } catch (Exception e) {
--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,9 +1,12 @@
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 
 @Entity
 @Getter
+@Setter
 public class Member {
+  long id;
   String name;
 }
