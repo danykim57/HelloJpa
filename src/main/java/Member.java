16d6c09ea271a2e8e8e6d966c4c6af071b11d722
YeonginKim <fhtmah12@gmail.com>
2023-01-07 22:12:48 +0900

    schema

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -2,11 +2,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "MBR")
 @Getter
 @Setter
 public class Member {
+  @Id
   long id;
   String name;
 }
