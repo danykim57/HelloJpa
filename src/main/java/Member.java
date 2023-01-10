671ef20a102bb9c53961ac498aca18f9ca91a2d2
YeonginKim <fhtmah12@gmail.com>
2023-01-10 22:04:48 +0900

    member edited

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,15 +1,22 @@
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@Getter
-@Setter
 public class Member {
   @Id
-  long id;
-  String name;
+  private Long id;
+  @Column(name = "name")
+  private String username;
+  private Integer age;
+  @Enumerated(EnumType.STRING)
+  private RoleType roleType;
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createdDate;
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastModifiedDate;
+  @Lob
+  private String description;
 }
