4cdd393f1c93df45c0e3776bd1ddfe5b65d1e184
YeonginKim <fhtmah12@gmail.com>
2023-01-19 21:51:41 +0900

    Inheritance concept

---
+++ src/main/java/Item.java
@@ -0,0 +1,12 @@
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Item {
+
+  @Id @GeneratedValue
+  private Long id;
+
+  private String name;
+  private int price;
+}
