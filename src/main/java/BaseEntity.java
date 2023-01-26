0042f917de281627f31261d6810b3ab615051512
YeonginKim <fhtmah12@gmail.com>
2023-01-26 21:59:04 +0900

    @Column added

--- src/main/java/BaseEntity.java
+++ src/main/java/BaseEntity.java
@@ -1,11 +1,13 @@
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 public class BaseEntity {
-
+  @Column(name = "INSERT_MEMBER")
   private String createdBy;
   private LocalDateTime createdDate;
+  @Column(name = "UPDATE_MEMBER")
   private String lastModifiedBy;
   private LocalDateTime lastModifiedDate;
 
